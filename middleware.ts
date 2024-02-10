cd6e0c2cd5509ec277210cfc28c160d8a772a638
Gagan Kumar <gagan.kumar@dimension64.com>
2024-02-10 19:42:21 +0530

    Add middleware

---
+++ middleware.ts
@@ -0,0 +1,12 @@
+// middleware.js
+import { NextRequest, NextResponse } from 'next/server';
+
+export function middleware(request: NextRequest) {
+    // Check if the request path starts with "/api/"
+    if (request.nextUrl.pathname.startsWith('/api/')) {
+        // Respond with an error message
+        return NextResponse.json({ success: false, message: 'Access denied' }, { status: 403 });
+    }
+    // If the request is not an API route, allow it to proceed
+    return NextResponse.next();
+}
